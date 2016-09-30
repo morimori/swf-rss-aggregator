308a55a687076f64d7cf2e677e7c801c108f6a23
Takatoshi MORIYAMA <hawk@at-exit.com>
2016-09-30 12:53:18 +0900

    RSS アグリゲーターワークフロースターターのモック

---
+++ bin/rss_aggregator_workflow_starter.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/env ruby
+
+require_relative '../lib/utils'
+require 'rss_aggregator_workflow'
+
+domain = setup_domain('example_rss_aggregator')
+
+client = AWS::Flow::workflow_client(domain.client, domain) { { from_class: 'RssAggregatorWorkflow' } }
+client.start_execution
